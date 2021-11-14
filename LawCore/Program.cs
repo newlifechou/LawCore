ac5c0ed75c79db750fad55b97f645184cbb0b016
xs.zhou <xs.zhou@outlook.com>
2021-11-14 19:47:30 +0800

    添加项目文件。

---
+++ LawCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LawCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
