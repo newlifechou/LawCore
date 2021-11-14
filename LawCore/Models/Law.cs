af01082a7f50a9f65e2ce6bac8c61824b3d08c62
xs.zhou <xs.zhou@outlook.com>
2021-11-14 20:17:15 +0800

    add some model class

---
+++ LawCore/Models/Law.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawCore.Models
+{
+    public class Law
+    {
+        public int Id { get; set; }
+        public string LawName { get; set; }
+        public string LawFullName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime LastUpdateTime { get; set; }
+    }
+}
---
+++ LawCore/Models/LawItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawCore.Models
+{
+    public class LawItem
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// article, section, subsection, paragraph, subparagraph, item. clause,rule,regulation,stipulation
+        /// </summary>
+        public string Category { get; set; }
+        public string Content { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+    }
+}
